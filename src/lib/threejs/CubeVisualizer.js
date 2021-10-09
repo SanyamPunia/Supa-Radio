48a7d3cb23038dab026378a0a2b7a19ff4f44b29
Zeldariomon <puniayash@gmail.com>
2021-10-09 20:34:56 +0530

    initiated cube visualizer

--- src/lib/threejs/CubeVisualizer.js
+++ src/lib/threejs/CubeVisualizer.js
@@ -4,16 +4,13 @@ import * as Utility from '$lib/utility'
 export default class CubeVisualizer {
     constructor(scene) {
         this.scene = scene;
-        this.cube = new THREE.Mesh(
-            new THREE.BoxBufferGeometry(1, 1, 1, 24, 24, 24),
-            new THREE.MeshPhongMaterial({color: 0xff0066})
-        )
-        this.cube.position.set(0,0,-2);
-        this.scene.add(this.cube);
+        this.initScene();
     }
 
     animate(timeDelta, dataArray) {
-        this.cube.rotateY(timeDelta * dataArray[5] * 0.00001);
     }
 
+    initScene() {
+        
+    }
 }
